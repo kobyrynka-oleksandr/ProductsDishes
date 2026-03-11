2914e3a19ac26e73de39ad61851e5d1ea4d1a47d
Oleksandr_Kobyrynka <kobyrynka.oleksandr1123@vu.cdu.edu.ua>
2026-03-11 12:24:57 +0200

    Layout of the EditDishIngradientWindow added

---
+++ ProductsDishes/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductsDishes.DataAccess.Postgres.Models;
+using ProductsDishes.DataAccess.Postgres.Repositories;
+
+namespace ProductsDishes
+{
+    internal class MainWindowViewModel
+    {
+    }
+}
